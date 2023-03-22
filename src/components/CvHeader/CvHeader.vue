7155c8098d0e7cdebb8b95c1434488dbd5259dda
Harut <har.amirkhanyan@gmail.com>
2023-03-23 02:20:58 +0400

    Add Header &headerNav

---
+++ src/components/CvHeader/CvHeader.vue
@@ -0,0 +1,22 @@
+<template lang='pug'>
+.cvHeader 
+  .cvHeader__name Hello! My name is Harut and I am
+  h1.cvHeader__title {{title}}
+
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'CvHeader',
+  setup() {
+  const title=ref('FRONTEND & WORDPRESS DEVELOPER')
+    return {
+      title,
+
+    };
+  },
+};
+</script>
+
+<style src='./_cvHeader.scss' lang='scss'></style>
---
+++ src/components/CvHeaderNav/CvHeaderNav.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+.cvHeaderNav
+  .cvHeaderNav__wrapper
+    h1.cvHeaderNav__name {{name}}
+    ul.cvHeaderNav__menu
+      li.cvHeaderNav__item(v-for="item in items" :key="item.id") {{item.text}}
+    span.cvHeaderNav__line
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'CvHeader',
+  setup() {
+    const name = ref('Harutyun');
+    const nav = ref([
+      { id: 'home', text: 'Home' },
+      { id: 'aboutMe', text: 'About me' },
+      { id: 'experience', text: 'Experience' },
+      { id: 'education', text: 'Education' },
+      { id: 'skills', text: 'Skills' },
+      { id: 'portfolio', text: 'Portfolio' },
+      { id: 'contacts', text: 'Contacts' },
+    ]);
+    return {
+      nav,
+      name
+    };
+  },
+};
+</script>
+
+<style src="./_cvHeaderNav.scss" lang="scss"></style>
