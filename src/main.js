8f7a9fd76e6b20785c6c2381454af402cd5c896d
Harut <har.amirkhanyan@gmail.com>
2023-06-15 15:06:40 +0400

    build

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import '@/assets/scss/_main.scss'
-import store from './store'
-import scrollTo from '@/plugins/scrollTo';
-
-
-createApp(App).use(store).use(scrollTo).mount('#app')
